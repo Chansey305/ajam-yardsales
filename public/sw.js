fe2e2e7134f4fbc3ec9bb6ac5405154923c1fecb
Chansey305 <chansey305@users.noreply.github.com>
2026-09-17 14:48:01 -0500

    ui: welcome bio in light green box above sale photos

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 /* AJAM Yardsales service worker */
-const CACHE = 'ajam-yardsales-v10';
+const CACHE = 'ajam-yardsales-v11';
 const PRECACHE = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png', '/icons/apple-touch-icon.png'];
 
 self.addEventListener('install', (event) => {
