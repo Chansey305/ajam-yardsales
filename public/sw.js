0e4fd41d2fd3aad7cb1362212cce4985164616fb
Chansey305 <chansey305@users.noreply.github.com>
2026-09-17 13:37:53 -0500

    ui: make Unsubscribe button red

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 /* AJAM Yardsales service worker */
-const CACHE = 'ajam-yardsales-v7';
+const CACHE = 'ajam-yardsales-v8';
 const PRECACHE = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png', '/icons/apple-touch-icon.png'];
 
 self.addEventListener('install', (event) => {
